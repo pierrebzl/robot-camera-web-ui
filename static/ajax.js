a2c9fdbbd5a9c1eaa7921b516803f9c3d5ca1a9e
Pierre Bonzel <pierre.bonzel@gmail.com>
2017-04-05 19:07:52 -0400

    rm console.log

--- static/ajax.js
+++ static/ajax.js
@@ -3,28 +3,24 @@ $(document).ready(function() {
                 async: true });
 
   $('button#forward').bind('click', function() {
-	console.log("click");
     $.ajax({
       url: '/forward'
     });
   });
 
   $('button#stop').bind('click', function() {
-	console.log("click");
     $.ajax({
       url: '/stop'
     });
   });
 
   $('button#backward').bind('click', function() {
-	console.log("click");
     $.ajax({
       url: '/backward'
     });
   });
 
   $('button#takePic').bind('click', function() {
-	console.log("click");
     $.ajax({
       url: '/take_pic',
       success: function(response) {
@@ -42,29 +38,9 @@ $(document).ready(function() {
   });
 
   $('button#recordVid').bind('click', function() {
-	console.log("click");
     $.ajax({
       url: '/record_vid'
     });
   });
 
 });
-
-
-/*$(function() {
-	$('button#Avancer').bind('click', function() {
-		$.getJSON($SCRIPT_ROOT + '/Avancer', function(data, item){
-	         $( "#result" ).text(data.result);
-	      });
-	    return false;
-	});
-});
-
-$(function() {
-	$('button#Reculer').bind('click', function() {
-		$.getJSON($SCRIPT_ROOT + '/Reculer', function(data, item){
-	         $( "#result" ).text(data.result);
-	      });
-	    return false;
-	});
-});*/
